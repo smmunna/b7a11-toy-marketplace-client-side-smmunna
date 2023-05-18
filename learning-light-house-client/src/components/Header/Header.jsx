d47b559cef852be0eb32bc3f05d9589c9ee6317e
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-18 09:20:59 +0600

    Header complete

--- learning-light-house-client/src/components/Header/Header.jsx
+++ learning-light-house-client/src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+import Logo from '../../assets/favicon/fav_ico.png'
+import ActiveLink from '../ActiveLink/ActiveLink';
 
 const Header = () => {
+    const navLink = <>
+        <li><ActiveLink to="/">Home</ActiveLink></li>
+        <li><ActiveLink to="/alltoys">All Toys</ActiveLink></li>
+        <li><ActiveLink to="/addtoys">Add Toys</ActiveLink></li>
+        <li><ActiveLink to="/mytoys">My Toys</ActiveLink></li>
+        <li><ActiveLink to="/blogs">Blogs</ActiveLink></li>
+    </>
     return (
-        <div>
-            <h3>Header component</h3>
+        <div className="navbar py-3 lg:px-20">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="smscreenmenu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {navLink}
+                    </ul>
+                </div>
+                <img src={Logo} width={40} alt="LightHouse" /> <h3 className="brand pt-2 ml-8 md:ml-0 lg:ml-2 text-xl lg:text-3xl font-semibold">Learning Lighthouse</h3>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {navLink}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link to="/login" className='button p-2 text-white rounded w-32 text-center hover:bg-warning'>Login</Link>
+            </div>
         </div>
     );
 }
