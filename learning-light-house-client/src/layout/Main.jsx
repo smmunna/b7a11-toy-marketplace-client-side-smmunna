4c5f7250000c02c6962376dfe07974454799632b
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-18 10:11:57 +0600

    footer added

--- learning-light-house-client/src/layout/Main.jsx
+++ learning-light-house-client/src/layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Header/>
             <Outlet/>
+            <Footer/>
             <ScrollRestoration/>
         </div>
     );
