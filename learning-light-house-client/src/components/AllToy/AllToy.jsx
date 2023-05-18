ab6ebed3780631826109a20db24983a3547cfb24
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-18 20:51:18 +0600

    Search functionality added

--- learning-light-house-client/src/components/AllToy/AllToy.jsx
+++ learning-light-house-client/src/components/AllToy/AllToy.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import ToyTable from './ToyTable';
 import './ToyTable.css'
 
 const AllToy = () => {
     const datas = useLoaderData()
+    const [mytoysData, setmytoysData] = useState(datas)
 
     const handleSearchSubmit = event => {
         event.preventDefault()
+        const form = event.target;
+        const searchText = form.searchText.value;
+        fetch(`https://b7a11-toy-marketplace-server-side-smmunna.vercel.app/toys/search?name=${searchText}`)
+            .then(res => res.json())
+            .then(data => setmytoysData(data))
     }
     return (
         <div className='px-5 md:px-20 my-3'>
@@ -15,8 +21,8 @@ const AllToy = () => {
             <hr />
             <div className='py-4'>
                 <form onSubmit={handleSearchSubmit}>
-                    <input type="text" className='toy-search p-2' placeholder='search by name' />
-                    <button className='btn btn-info ml-2'>Search</button>
+                    <input type="text" name='searchText' className='toy-search p-2' placeholder='Search by name' />
+                    <button className='mysearchbtn ml-2'>Search</button>
                 </form>
             </div>
             <div className="overflow-x-auto">
@@ -36,12 +42,22 @@ const AllToy = () => {
                     <tbody>
 
                         {
-                            datas.map((data, index) => (
-                                <ToyTable
-                                    data={data}
-                                    index={index}
-                                />
-                            ))
+                            mytoysData.length > 0 ?
+                                <>
+                                    {
+                                        mytoysData.map((data, index) => (
+                                            <ToyTable
+                                                key={data._id}
+                                                data={data}
+                                                index={index}
+                                            />
+                                        ))
+                                    }
+                                </>
+                                :
+                                <>
+                                    <div className='text-2xl text-red-700 my-5'>No data found !</div>
+                                </>
                         }
 
                     </tbody>
